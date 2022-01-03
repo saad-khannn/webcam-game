6aaffa957a7a07d700dd6b1bde5cabfcd5d65d8d
saad <saadkhan12313@yahoo.com>
2022-01-03 17:30:25 -0500

    flip/mirror webcam video

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     cv2.imshow('webcam', frame)
     if cv2.waitKey(1) == ord('q'):
         break
