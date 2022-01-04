fea8567ace2ce1a886986662732e68d32480aeeb
saad <saadkhan12313@yahoo.com>
2022-01-04 18:30:43 -0500

    timer and placeholder score

--- game.py
+++ game.py
@@ -3,14 +3,14 @@ import numpy as np
 import time
 
 proto = "models/deploy.prototxt.txt"
-weights = "models/res10_300x300_ssd_iter_140000.caffemodel"
+weights = "models/res10_300x300_ssd_iter_140000.caffemodel" 
 
 net = cv2.dnn.readNetFromCaffe(proto, weights)
 cap = cv2.VideoCapture(0) 
-timer = 10 
+start = time.time() 
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() 
     frame = cv2.flip(frame, 1) 
     h, w = frame.shape[:2] 
     
@@ -19,23 +19,28 @@ while True:
     faces = net.forward() 
 
     for i in range(faces.shape[2]):
-            confidence = faces[0, 0, i, 2]
-            if confidence > 0.5: 
-                box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (x, y, x1, y1) = box.astype("int")
-                cv2.rectangle(frame, (x,y), (x1,y1), (0,0,255), 3)
-    
-    if timer > 0: 
-        time.sleep(1)
-        timer -= 1 
-        cv2.putText(frame, str(timer), (200,h-10), 
-        cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 5, cv2.LINE_AA)
+        confidence = faces[0, 0, i, 2]
+        if confidence > 0.5:
+            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (x, y, x1, y1) = box.astype("int")
+            cv2.rectangle(frame, (x,y), (x1,y1), (0,0,255), 3)
+
+    end = time.time() 
+    elapsed = int(end - start) 
+    timer = 10 - elapsed 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    if timer > 0:
+        cv2.putText(frame, 'Timer:' + str(timer), 
+        (5,30), font, 1, (255,255,255), 1, cv2.LINE_AA)
     else:
-        cv2.putText(frame, 'DONE', (200,h-10), 
-        cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 5, cv2.LINE_AA)
+        cv2.putText(frame, 'ROUND OVER', (5,30), 
+        font, 1, (255,255,255), 2, cv2.LINE_AA)
     
-    cv2.imshow('webcam', frame) 
-    if cv2.waitKey(1) == ord('q'): 
+    cv2.putText(frame, 'Score:' + str(elapsed), 
+    (int(w*0.72),30), font, 1, (255,255,255), 1, cv2.LINE_AA)
+    
+    cv2.imshow('webcam', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
