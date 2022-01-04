3ebf01323fb65cc80a5c4701a9fe922bb88a4060
saad <saadkhan12313@yahoo.com>
2022-01-03 19:05:08 -0500

    haar cascades face detection

--- game.py
+++ game.py
@@ -1,10 +1,18 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+        roi_gray = gray[y:y+w, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
     cv2.imshow('webcam', frame)
     if cv2.waitKey(1) == ord('q'):
         break
