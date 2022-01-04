1c65d56c0f0dca44c7ea6085884366eb6dd752bc
saad <saadkhan12313@yahoo.com>
2022-01-03 19:06:28 -0500

    red face detection box

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.05, 5)
     for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
