e4f5a49f0d913783e54cab559dca132700be1550
saad <saadkhan12313@yahoo.com>
2022-01-05 19:18:53 -0500

    formatting

--- game.py
+++ game.py
@@ -3,66 +3,64 @@ import numpy as np
 import time
 from random import randrange
 
-proto = "models/deploy.prototxt.txt"
-weights = "models/res10_300x300_ssd_iter_140000.caffemodel"
-
+proto = "models/deploy.prototxt.txt" 
+weights = "models/res10_300x300_ssd_iter_140000.caffemodel" 
 net = cv2.dnn.readNetFromCaffe(proto, weights) 
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) 
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) 
 
 start = time.time() 
 coordinates = False 
 score = 0 
-x1,y1,x2,y2 = 0,0,1,1 
+x1, y1, x2, y2 = 0, 0, 1, 1 
 
 def circleHit(rectX1, rectY1, rectX2, rectY2, circleWidth, circleHeight):
-    width = range(circleWidth-25, circleWidth+25) 
-    height = range(circleHeight-25, circleHeight+25) 
-    xRange = range(rectX1, rectX2)
+    width = range(circleWidth - 25, circleWidth + 25)
+    height = range(circleHeight - 25, circleHeight + 25) 
+    xRange = range(rectX1, rectX2) 
     yRange = range(rectY1, rectY2) 
-    if(range(max(width[0],xRange[0]), min(width[-1],xRange[-1])) 
-    and range(max(height[0],yRange[0]), min(height[-1],yRange[-1]))):
+    if(range(max(width[0], xRange[0]), min(width[-1], xRange[-1])) 
+    and range(max(height[0], yRange[0]), min(height[-1], yRange[-1]))):
         return True 
 
 while True:
     ret, frame = cap.read() 
     frame = cv2.flip(frame, 1) 
-    h, w = frame.shape[:2] 
-    frame[0:40, 0:w] = (0,0,0)
+    height, width = frame.shape[:2] 
+    frame[0:40, 0:width] = (0, 0, 0)
     
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300,300), (104.0,177.0,123.0))
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
     net.setInput(blob) 
     faces = net.forward() 
-
     for i in range(faces.shape[2]):
         confidence = faces[0, 0, i, 2]
         if confidence > 0.5: 
-            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+            box = faces[0, 0, i, 3:7] * np.array([width, height, width, height])
             (x1, y1, x2, y2) = box.astype("int")
-            face = cv2.rectangle(frame, (x1, y1), (x2, y2), (0,0,255), 3)
+            face = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-    end = time.time() 
-    elapsed = end - start 
-    timer = int(61 - elapsed) 
+    end = time.time()
+    elapsed = end - start
+    timer = int(60 - elapsed)
     font = cv2.FONT_HERSHEY_SIMPLEX
     if timer > 0:
-        cv2.putText(frame, 'Timer:' + str(timer), (5,30), font, 1, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Timer:' + str(timer), (5, 30), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
         if coordinates == False: 
-            randomWidth = randrange(25, w-25)
-            randomHeight = randrange(70, h-25)
-            coordinates = (randomWidth, randomHeight) 
-        circle = cv2.circle(frame, coordinates, 25, (0,0,255), -1) 
+            randomWidth = randrange(25, width - 25)
+            randomHeight = randrange(65, height - 25)
+            coordinates = (randomWidth, randomHeight)
+        circle = cv2.circle(frame, coordinates, 25, (0, 0, 255), -1) 
         if circleHit(x1, y1, x2, y2, coordinates[0], coordinates[1]):
             coordinates = False 
             score += 1
     else:
-        cv2.putText(frame, "GAME OVER", (5,30), font, 1, (255,255,255), 3, cv2.LINE_AA)
-        cv2.putText(frame, 'Score:' + str(score), (int(w*0.74),30), font, 1, (255,255,255), 3, cv2.LINE_AA) 
-    
-    cv2.putText(frame, 'Score:' + str(score), (int(w*0.74),30), font, 1, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "GAME OVER", (5, 30), font, 1, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(frame, 'Score:' + str(score), (int(width * 0.74), 30), font, 1, (255, 255, 255), 3, cv2.LINE_AA) 
+    cv2.putText(frame, 'Score:' + str(score), (int(width * 0.74), 30), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
-    cv2.imshow('webcam', frame)
+    cv2.imshow('webcam', frame) 
     if cv2.waitKey(1) == ord('q'): 
         break
 
